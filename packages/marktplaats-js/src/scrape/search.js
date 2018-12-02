9d16c1e1cc5e49d91bf46bad48cf89dc99b886c6
Michiel Sikma <michiel@sikma.org>
2018-12-02 18:33:59 +0100

    Merge in changes from other updates

--- packages/marktplaats-js/src/scrape/search.js
+++ packages/marktplaats-js/src/scrape/search.js
@@ -5,7 +5,7 @@ import { cheerio } from 'mlib-common/lib/scrape'
 import { removeQuery } from 'mlib-common/lib/query'
 import requestURI from 'mlib-common/lib/request'
 
-import { parseStatus, parseDelivery, parsePrice } from './util'
+import { parsePrice, separateAttributes, addHttps } from './util'
 import { searchURI, makeURILocal } from './uris'
 
 // Runs search page scraping code on the passed HTML string.
@@ -17,9 +17,13 @@ const scrapeResults = (html) => {
 // Retrieves all results from the result page minus any sponsored results that may be there.
 const getRealResults = ($) => {
   const results = $('.search-results-table article.search-result:not(.bottom-listing)')
-  return results.get().map(result => {
+  return results.get().reduce((all, result) => {
     try {
       const $result = $(result)
+      // Make sure this isn't an advertisement result.
+      const priority = $('.mp-listing-priority-product', result).text().trim()
+      if (priority === 'Topadvertentie') return all
+
       const title = $('.heading', result).text().trim()
       const desc = $('.description > span', result).get().map(d => $(d).text().trim()).join(' ')
       const url = removeQuery($result.attr('data-url').trim())
@@ -27,16 +31,22 @@ const getRealResults = ($) => {
       const id = $result.attr('data-item-id').trim()
       const priceRaw = $('.column-price .price-new', result).text().trim()
       const priceVals = parsePrice(priceRaw)
-      const $listingAttributes = $('.listing-priority-product-container .mp-listing-attributes', result)
-      const status = parseStatus($($listingAttributes[0]).text().trim())
-      const delivery = parseDelivery($($listingAttributes[1]).text().trim())
+      const $listingAttr = $('.listing-priority-product-container .mp-listing-attributes', result)
+      const attributes = separateAttributes([$($listingAttr[0]).text().trim(), $($listingAttr[1]).text().trim()])
+      const status = attributes['_status'] || null
+      const delivery = attributes['_delivery'] || null
+      const $thumb = $('.listing-image img', result)
+      const thumbSrc = addHttps($thumb.attr('data-img-src'))
+      const thumbAlt = addHttps($thumb.attr('src'))
+      const thumb = thumbSrc ? thumbSrc.trim() : thumbAlt ? thumbAlt.trim() : null
+      const hasThumb = thumb ? !/no_photo\.jpg$/.test(thumb) : null
       const location = $('.location-name', result).text().trim()
       const $seller = $('.seller-name a', result)
       const seller = {
         name: $seller.text().trim(),
         url: $seller.attr('href').trim()
       };
-      return {
+      return [...all, {
         id,
         title,
         desc,
@@ -46,14 +56,16 @@ const getRealResults = ($) => {
         ...priceVals,
         seller,
         delivery,
-        status
-      }
+        status,
+        thumb,
+        hasThumb
+      }]
     }
     catch (error) {
       // If something went wrong, skip only this result.
       return { errorType: 'PARSE_ERROR', error }
     }
-  })
+  }, [])
 }
 
 // Runs a search query, extracts the information, and then returns it.
