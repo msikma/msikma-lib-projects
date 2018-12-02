168af57b05018ac45193ed289709c25b620dcba0
Michiel Sikma <michiel@sikma.org>
2018-12-02 18:02:06 +0100

    Add function for trimming strings with specific characters

--- packages/mlib-common/src/util.js
+++ packages/mlib-common/src/util.js
@@ -3,3 +3,13 @@
 
 // Expose array-flatten: https://www.npmjs.com/package/array-flatten
 export { default as flatten } from 'array-flatten'
+
+// Trims a specific character from a string like trim() does with whitespace.
+// Taken from Stack Overflow and modified slightly:
+// <https://stackoverflow.com/a/32516190>
+export const charTrim = (str, replChar = '') => {
+  let oldChar = replChar
+  if (oldChar === "]") oldChar = "\\]";
+  if (oldChar === "\\") oldChar = "\\\\";
+  return str.replace(new RegExp(`^["${oldChar}"]+|["${oldChar}"]+$`, 'g'), '');
+}
