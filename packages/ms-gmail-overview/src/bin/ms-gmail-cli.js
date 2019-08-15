40b367651d51ada88b0a9e2844bc5fb6d9220ac5
Michiel Sikma <michiel@sikma.org>
2019-08-15 19:37:47 +0200

    Add ability to include 'read' items (skip them by default)

--- packages/ms-gmail-overview/src/bin/ms-gmail-cli.js
+++ packages/ms-gmail-overview/src/bin/ms-gmail-cli.js
@@ -12,18 +12,24 @@ const parser = makeArgParser({
   addHelp: true,
   description: ensurePeriod(packageData.description),
   epilog: `For more information, see <${packageData.homepage}>.`,
-  version: packageData.version
+  version: packageData.version,
+  longHelp: `This script uses an SQLite database to keep track of which items have
+already been shown. The 'data' action always returns all items, but 'list'
+only returns new items by default: items we haven't cached yet, and items
+that are set to 'read' remotely.
+`
 })
 
 parser.addArgument(['-q', '--quiet'], { help: 'Disables output printing (used to cache data).', action: 'storeTrue' })
 parser.addArgument(['--action'], { help: 'Which action to take.', choices: ['data', 'list'] })
-parser.addArgument(['--output'], { help: 'Result output format.', choices: ['json', 'xml', 'terminal'], _choicesHelp: ['JSON string', 'XML string', 'Plain text readable in terminal (default)'], metavar: 'TYPE', defaultValue: 'terminal' })
+parser.addArgument(['--output'], { help: 'Result output format (for \'data\' action).', choices: ['json', 'xml', 'terminal'], _choicesHelp: ['JSON string', 'XML string', 'Plain text readable in terminal (default)'], metavar: 'TYPE', defaultValue: 'terminal' })
+parser.addArgument(['--include-read'], { help: 'Includes read emails when using the \'list\' action.', action: 'storeTrue' })
 parser.addArgument(['--cache-only'], { help: 'Refreshes cache without printing output.', action: 'storeTrue' })
-parser.addArgument(['--no-cache'], { help: 'Forces a cache-less request.', action: 'storeTrue' })
-parser.addArgument(['--db-loc'], { help: 'Location of the SQLite database for storing read information.', defaultValue: '~/.cache/ms-gmail-js/id-database.json', metavar: 'DBL' })
+parser.addArgument(['--no-cache'], { help: 'Forces a cache-less request (still saves to cache).', action: 'storeTrue' })
+parser.addArgument(['--db-loc'], { help: 'Location of the SQLite database for storing cache.', defaultValue: '~/.cache/ms-gmail-js/id-database.json', metavar: 'DBL' })
 parser.addArgument(['--cookie-loc'], { help: 'Location of the cookie authentication file.', defaultValue: '~/.config/ms-gmail-js/cookies.txt', metavar: 'CKL' })
 parser.addArgument(['--cache-loc'], { help: 'Location of the cached data.', defaultValue: '~/.cache/ms-gmail-js/gmail-cache.json', metavar: 'CL' })
-parser.addArgument(['--cache-time'], { help: 'Duration that the cache is considered valid, in minutes. By default: 300 (5 hours).', defaultValue: 300, metavar: 'MIN' })
+parser.addArgument(['--cache-time'], { help: 'Duration that the cache is considered valid, in minutes. By default: 5.', defaultValue: 5, metavar: 'MIN' })
 
 const parsed = parser.parseArgs()
 const action = parsed.action
--- packages/ms-gmail-overview/src/scrape/list.js
+++ packages/ms-gmail-overview/src/scrape/list.js
@@ -7,9 +7,11 @@ import { removeCached, cacheItems } from './cache'
 const separator = '@%@'
 
 /** Simple email output for consumption by shell scripts. */
-const listEmails = async (mails) => {
+const listEmails = async (mails, args) => {
+  const includeRead = !!args.include_read
   const newMails = await removeCached(mails)
-  for (const { timestamp, sender, link, preview, subject, id } of newMails) {
+  for (const { timestamp, read, sender, link, preview, subject, id } of newMails) {
+    if (read && !includeRead) continue
     console.log([id, timestamp, sender, subject, preview, link].join(separator))
   }
   await cacheItems(newMails.map(m => m.id))
