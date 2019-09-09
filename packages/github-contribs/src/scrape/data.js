5762c3f75c362d557acab9678c860ec7659fb542
Michiel Sikma <michiel@sikma.org>
2019-09-09 19:10:30 +0200

    Return user info in addition to contributions

--- packages/github-contribs/src/scrape/data.js
+++ packages/github-contribs/src/scrape/data.js
@@ -5,6 +5,7 @@ import cacheData, { retrieveCache } from 'mlib-common/lib/cache'
 import { sortObjKeys } from 'mlib-common/lib/sort'
 import requestURI from 'mlib-common/lib/request'
 import { cheerio } from 'mlib-common/lib/scrape'
+import { trimInner } from 'mlib-common/lib/text'
 
 import { userURL } from './uris'
 
@@ -32,7 +33,47 @@ const parseContribs = html => {
       }
     }
   }, {})
-  return { dates: sortObjKeys(cDates) }
+  
+  const $userInfo = $('.h-card')
+  const $avatar = $('img.avatar.width-full', $userInfo)
+  const avatarURL = $avatar.attr('src').trim()
+  const avatarWidth = Number($avatar.attr('width').trim())
+  const avatarHeight = Number($avatar.attr('height').trim())
+  const status = trimInner($('.user-status-container', $userInfo).text().trim())
+  const fullname = $('.vcard-fullname', $userInfo).text().trim()
+  const username = $('.vcard-username', $userInfo).text().trim()
+  const biography = $('.user-profile-bio', $userInfo).text().trim()
+  const organization = $('.vcard-detail .p-org', $userInfo).text().trim()
+  const location = $('.vcard-detail .p-label', $userInfo).text().trim()
+  const website = $($('.vcard-detail[data-test-selector="profile-website-url"]', $userInfo).get()[0]).text().trim()
+  
+  let cInfo
+  let cNumber
+  try {
+    cInfo = trimInner($('.js-yearly-contributions > div > h2').text().trim())
+  } catch (err) {
+  }
+  try {
+    cNumber = Number(cInfo.match(/[0-9,]+/)[0].replace(',', ''))
+  } catch (err) {
+  }
+  return {
+    dates: sortObjKeys(cDates),
+    info: { headline: cInfo, amount: cNumber },
+    user: {
+      username,
+      fullname,
+      biography,
+      website,
+      organization,
+      avatar: {
+        url: avatarURL,
+        width: avatarWidth,
+        height: avatarHeight
+      },
+      location
+    }
+  }
 }
 
 const reqGithubContribs = async (username) => {
