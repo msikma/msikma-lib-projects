95260e13edd705f76542d5b63204025e09125cd6
Michiel Sikma <michiel@sikma.org>
2020-07-03 16:45:46 +0200

    Adjust transpilation - no longer need regenerator

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env']
+    ['@babel/preset-env', {
+      'targets': {
+        'browsers': ['last 2 versions']
+      },
+      'exclude': [
+        'transform-regenerator'
+      ]
+    }]
   ],
   plugins: ['@babel/plugin-proposal-class-properties']
 }
